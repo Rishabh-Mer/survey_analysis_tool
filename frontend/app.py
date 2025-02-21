9f68760b25bc0cd31403af2f718f365260738513
Rishabh Mer <rishabhmer@Rishabhs-MacBook-Pro.local>
2025-02-21 00:33:07 +0000

    Web Application

---
+++ frontend/app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import json
+import requests
+import time
+
+from loguru import logger
+
+
+st.set_page_config(
+    page_title="Survey Analysis Tool",
+    page_icon="📊",
+)
+
+# inject_css()
+
+st.write('# Survey Analysis Tool')  
+
+st.markdown("Hello, this survey analysis tool.")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if query := st.chat_input("Enter your query here"):
+    st.chat_message("user", avatar="🧍🏻").markdown(query)
+    st.session_state.messages.append({"role": "user", "content": query})
+    
+    # Send query to backend
+    response = requests.post("http://localhost:8000/survey", json={"query": query})
+    response = response.json()
+    
+    # add emoji in spinner
+    with st.spinner(""):
+        time.sleep(2)    
+        st.chat_message("ai", avatar="🤖").markdown(response['answer'])
+        st.session_state.messages.append({"role": "assistant", "content": response['answer']})
+        
+    
+
+
